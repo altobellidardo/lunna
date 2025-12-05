b3771dfc6f5174d471d81ffcadb474d6c64ae521
Dardo Altobelli <altobellidardo@gmail.com>
2025-12-05 11:37:51 -0300

    feat: add GoogleStrategy provider to AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { AuthController } from './auth.controller'
+import { GoogleStrategy } from './strategies/google.stategy'
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  providers: [GoogleStrategy]
 })
 export class AuthModule {}
