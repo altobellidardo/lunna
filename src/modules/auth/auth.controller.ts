6404700cbac85506b3304c771424986de4402c02
Dardo Altobelli <altobellidardo@gmail.com>
2025-12-05 11:29:51 -0300

    feat: refactor GoogleAuthGuard import and remove unused AuthService from AuthController

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,8 @@
 import { Controller, Get, UseGuards } from '@nestjs/common'
-import { AuthService } from './auth.service'
-import { GoogleAuthGuard } from 'src/guards/google.guard'
+import { GoogleAuthGuard } from 'src/common/guards/google/google.guard'
 
 @Controller('auth')
 export class AuthController {
-  constructor (private readonly authService: AuthService) {}
-
   @UseGuards(GoogleAuthGuard)
   @Get('google/login')
   googleLogin () {
